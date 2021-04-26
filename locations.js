66766f255864886e106c88578e40d06cef5dcb4c
Nick Eggert <neggert124@gmail.com>
2021-04-26 13:29:19 -0400

    added locations to locations.js

---
+++ locations.js
@@ -0,0 +1,14 @@
+/* locations.js
+Nick Eggert
+
+Stores the location data for the map */
+
+location.insert({name : "Savitz Hall", info : "Admissions"}) // 1 on RowanCampusMap.png
+location.insert({name : "Campbell Library", info : "Library"}) // 2 on RowanCampusMap.png
+location.insert({name : "Recreation Center", info : "Health Club / Fitness Center"}) // 4 on RowanCampusMap.png
+location.insert({name : "Esbjornson Gym", info : "Gym connected to the Rec Center, Swimming Pool"}) // 5 on RowanCampusMap.png
+location.insert({name : "James Hall", info : "Home to the College of Education"}) // 6 on RowanCampusMap.png
+location.insert({name : "Robinson Hall", info : "Home to the Computer Science Department"}) // 7 on RowanCampusMap.png
+location.insert({name : "Wilson Hall", info : "Home to the College of Performing Arts"}) // 8 on RowanCampusMap.png
+location.insert({name : "Rowan Hall", info : "Home to the Henry M. Rowan College of Engineering"}) // 9 on RowanCampusMap.png
+location.insert({name : "Engineering Hall", info : "Additional Building for Engineering classes, connected by a third-floor bridge to Rowan Hall"}) // 10 on RowanCampusMap.png
