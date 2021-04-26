1f544501c0d2aada40f3ef8d9157b0144572d05a
Nick Eggert <neggert124@gmail.com>
2021-04-26 15:41:46 -0400

    updated ways to connect users to db

--- users.js
+++ users.js
@@ -4,7 +4,7 @@ creates users and implement login function, stores user data in DB. */
 
 const mongoose = require("mongoose");
 
-const UserSchema = mongoose.Schema({
+const UserSchema = new mongoose.Schema({
     username: {
         type: String,
         required: true
@@ -17,6 +17,18 @@ const UserSchema = mongoose.Schema({
         type: Date,
         default: Date.now()
     }
+});
+
+mongoose.connect(/*'mongodb://localhost:27017/studentDB', ////connect our db here*/ {useNewUrlParser: true, useUnifiedTopology: true});
+
+const User = mongoose.model("User", UserSchema);
+const newUser = new User();
+
+newUser.save((error)=>{
+    //validateSync will check any register validation rules on a Schema
+    error = newUser.validateSync();
+    console.log(error.message)
+    process.exit(1);
 });
 
 // export user with UserSchema
