95befad0e677eccf0974ad0b6090acf320213101
Anthony DeSantis <anthonydesantis@Anthonys-MacBook.local>
2021-04-26 22:57:20 -0400

    minor changes

--- server/rowan_server.js
+++ server/rowan_server.js
@@ -16,7 +16,7 @@ var server = http.createServer(function (req, res) {   //create web server
     
     }
     else if (req.url == "/login") {
-        
+
         res.writeHead(200, { 'Content-Type': 'text/html' });
         res.write('<html><body><p>This is login Page.</p></body></html>');
         res.end();
